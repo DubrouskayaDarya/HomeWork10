de1010ca0512224da4fa95e816ee69cc9a79f895
Дарья Дубровская <>
2022-01-16 20:00:03 +0300

    Task 10: Project added

---
+++ HomeWork10/HomeWork10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork10
+//
+//  Created by Дарья Дубровская on 16.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
